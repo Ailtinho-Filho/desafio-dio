d2910a3e7f76f6b8e9d48ac28548ed03be4279b3
Ailtinho-filho <ailton.rodrigofilho@gmail.com>
2025-04-12 16:42:12 -0300

    resolvi o 1 desafio da dio

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-let nome = "Ailton"
+let heroi = ["Ailton"]
+let xpatual = 200
 
-console.log("Olá mundo")
+console.log("Olá " + heroi)
+
+if (xpatual < 1000) {
+    console.log("Seu nível é Ferro")
+} else if (xpatual >= 1001 && xpatual <= 2000) {
+    console.log("Seu nível é Bronze")
+} else if (xpatual >= 2001 && xpatual <= 5000) {
+    console.log("Seu nível é Prata-Ouro")
+} else if (xpatual >= 5001 && xpatual <= 8000) {
+    console.log("Seu nível é Platina-Diamante")
+} else if (xpatual >= 8001 && xpatual <= 9000) {
+    console.log("Seu nível é Ascendente")
+} else if (xpatual >= 9001 && xpatual <= 10000) {
+    console.log("Seu nível é Imortal")
+} else if (xpatual >= 10001) {
+    console.log("Seu nível é Radiante")
+}
