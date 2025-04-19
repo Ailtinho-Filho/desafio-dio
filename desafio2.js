82307b98f7f8c033f836c2e01488265456c27996
Ailtinho-filho <ailton.rodrigofilho@gmail.com>
2025-04-19 10:09:05 -0300

    commit desafio opradores logicos

---
+++ desafio2.js
@@ -0,0 +1,15 @@
+let PoochVena = [ "Poochvena", 2, 'M', 13]
+let ZigZagoon = ["zigzagoon", 2, 'F', 13]
+let Dragonite = ["Dragonite", 5, 'M', 25]
+let Dragonite2 = ["Dragonite2",5,'F',24]
+let PoochVena2 = ["Poochvena",3,'F',15]
+let Wurhple = ["Wurhple",2,'M',14]
+let dano = 7
+Wurhple[3] = 14 - 7 
+
+console.log(PoochVena[0] + "Cadastrado com sucesso!")
+console.log(ZigZagoon[0] + "Cadastrado com sucesso!")
+console.log(Dragonite[0] + "Cadastrado com sucesso!")
+console.log(Dragonite2[0] + "Cadastrado com sucesso!")
+console.log(PoochVena2[0] + "Cadastrado com sucesso!")
+console.log(Wurhple[0] + "Cadastrado com sucesso!")
